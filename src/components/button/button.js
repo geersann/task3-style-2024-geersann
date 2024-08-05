9d8c65332079811505299a701fea696d3defa387
geersann <148795588+geersann@users.noreply.github.com>
2024-08-05 23:28:40 +0300

    update button css style

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
+const buttonStyles = {
+    color: 'rgb(18, 255, 18)',
+    backgroundColor: 'transparent',
+    border: '0px',
+    fontFamily: 'Courier',
+    fontSize: '33pt'
+};
+
 export default function Button() {
     return (
         <button 
             data-testid="element-button"
-            style={{
-                color: 'rgb(18, 255, 18)',
-                backgroundColor: 'transparent',
-                border: '0px',
-                fontFamily: 'Courier',
-                fontSize: '33pt'
-            }}
+            style={buttonStyles}
         >
             Wake up Neo...
         </button>
